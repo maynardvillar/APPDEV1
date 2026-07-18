1956cca68dbef6e70d7b0677f058f77d0121699a
maynardvillar <maynardl.villar@gmail.com>
2026-07-18 18:03:01 +0800

    Done with laboratory 3

---
+++ L03_getting-started-react/src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="about-me">
+        <h1>About Me</h1>
+
+        <h2>Full Name</h2>
+        <p>Maynard L. Villar</p>
+
+        <h2>Course and Year</h2>
+        <p>BSIS 3 (BS Information Systems, 3rd Year)</p>
+
+        <h2>Fun Fact</h2>
+        <p>I'm a bit of an overthinker — I can spend way too long worrying about small details, even in code!</p>
+
+        <hr />
+
+        <h2>My Reflection</h2>
+        <p>
+          Learning Git and React this term has been a mix of confusing and exciting.
+          At first commands like git rebase and git stash felt overwhelming and I
+          made a lot of mistakes along the way like messing up commit messages or
+          forgetting which branch I was on. But going through the Git Dojo activity
+          taught me that mistakes are part of the process and commands like git
+          reset. Moving on to React
+          felt like a fresh challenge since I had to think differently about how a
+          web page is built using components instead of plain HTML. I still have a
+          lot to learn especially in organizing my code and thinking through
+          problems instead of overthinking every small step but I'm starting to
+          feel more confident with each activity. I hope to carry this patience and
+          problem solving mindset into a future career in Information Systems,
+          where I can help build reliable and user-friendly systems.
+        </p>
+      </section>
+    </>
+  )
+}
+
+export default App
